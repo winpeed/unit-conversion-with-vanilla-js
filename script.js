7c74c87b14ac7cd179abba4def7174825d4ce551
Praise Obende <davidobende@gmail.com>
2022-02-16 18:39:04 +0100

    Modify input logic when empty

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const literGallon = document.getElementById("liter-gallon");
 const kiloPound = document.getElementById("kilo-pound");
 
 userInput.value = 10;
+
+let convertNumber;
+
 doCalculation();
 
 userInput.addEventListener("change", doCalculation);
@@ -12,24 +15,26 @@ userInput.addEventListener("keyup", doCalculation);
 
 function doCalculation() {
   if (userInput.value == "") {
-    userInput.value = 0;
+    convertNumber = 0;
   }
 
-  meterFeet.innerHTML = ` <p>${userInput.value} meters = ${(
-    Number(userInput.value) * 3.2808399
-  ).toFixed(3)} feet <br/> ${userInput.value} feet = ${(
-    Number(userInput.value) / 3.2808399
+  convertNumber = userInput.value;
+
+  meterFeet.innerHTML = ` <p>${Number(convertNumber)} meters = ${(
+    Number(convertNumber) * 3.2808399
+  ).toFixed(3)} feet <br/> ${Number(convertNumber)} feet = ${(
+    Number(convertNumber) / 3.2808399
   ).toFixed(3)} meters</p>`;
 
-  literGallon.innerHTML = ` <p>${userInput.value} liters = ${(
-    Number(userInput.value) * 0.264172052
-  ).toFixed(3)} gallons <br/> ${userInput.value} gallons = ${(
-    Number(userInput.value) / 0.264172052
+  literGallon.innerHTML = ` <p>${Number(convertNumber)} liters = ${(
+    Number(convertNumber) * 0.264172052
+  ).toFixed(3)} gallons <br/> ${Number(convertNumber)} gallons = ${(
+    Number(convertNumber) / 0.264172052
   ).toFixed(3)} liters</p>`;
 
-  kiloPound.innerHTML = ` <p>${userInput.value} kilos = ${(
-    Number(userInput.value) * 2.20462262
-  ).toFixed(3)} pounds <br/> ${userInput.value} pounds = ${(
-    Number(userInput.value) / 2.20462262
+  kiloPound.innerHTML = ` <p>${Number(convertNumber)} kilos = ${(
+    Number(convertNumber) * 2.20462262
+  ).toFixed(3)} pounds <br/> ${Number(convertNumber)} pounds = ${(
+    Number(convertNumber) / 2.20462262
   ).toFixed(3)} kilos</p>`;
 }
